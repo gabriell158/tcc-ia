7586e7a0a9f4ff045182d161d1b659422e0788f2
Aicha Tassa <aichatassa@gmail.com>
2023-04-18 14:49:16 -0300

    adjusting train function to receive dataframe

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request
+from src.AI.train import train
+from src.AI.duplicated import duplicated
+import pandas as pd
+
+
+app = Flask(__name__)
+@app.route("/model",methods=["POST","GET"])
+def model1():
+  if request.method == "POST":
+    df = pd.read_csv('teste.csv')
+
+    new_df = duplicated(df)
+    teste = train(new_df)
+    #teste = train(request.data)
+    return teste
+  if request.method == "GET":
+    # listar os modelos treinados
+    return
+
+@app.route("/model/<int:model_id>",methods=["POST","DELETE"])
+def model2():
+  if request.method == "POST":
+    # seleciona o modelo para ser usado para inferencia
+    return
+  if request.method == "DELETE":
+    # exclui o modelo
+    return
+
+@app.route("/tracking",methods=["POST"])
+def tracking():
+  if request.method == "POST":
+    # faz a inferencia e salva no banco o resultado
+    # retorna o resultado da inferencia
+    return
+
+@app.route("/form",methods=["GET"])
+def form():
+  if request.method == "GET":
+    # lista os formulários preenchidos
+    return
--- src/AI/train.py
+++ src/AI/train.py
@@ -3,7 +3,36 @@ from src.AI.elbow import elbow_method
 from src.AI.raw_data import rawData, catCols, numCols
 from src.AI.denormalize import denormalize
 
-def train(gender, marital, university, ocupation, children, class_dep, class_anx, class_str, age, grad_period, s1, a2, d3, a4, d5, s6, a7, s8, a9, d10, s11, s12, d13, s14, a15, d16, d17, s18, a19, a20, d21):
+def train(df, class_dep, class_anx, class_str):
+    gender = df['Gender']
+    age = df['Age']
+    marital = df['Marital_Status']
+    university = df['University']
+    ocupation = df['Ocupation']
+    children = df['Children']
+    grad_period = df['Grad_Period']
+    s1 =  df['S1']
+    a2 =  df['A2']
+    d3 =  df['D3']
+    a4 =  df['A4']
+    d5 =  df['D5']
+    s6 =  df['S6']
+    a7 =  df['A7']
+    s8 =  df['S8']
+    a9 =  df['A9']
+    d10 =  df['D10']
+    s11 =  df['S11']
+    s12 =  df['S12']
+    d13 =  df['D13']
+    s14 =  df['S14']
+    a15 =  df['A15']
+    d16 =  df['D16']
+    d17 =  df['D17']
+    s18 =  df['S18']
+    a19 =  df['A19']
+    a20 =  df['A20']
+    d21 =  df['D21']
+
     cat_cols = catCols(gender, marital, university, ocupation)
     num_cols = numCols(age, grad_period, s1, a2, d3, a4, d5, s6, a7, s8, a9, d10, s11, s12, d13, s14, a15, d16, d17, s18, a19, a20, d21)
     cat_normalized = catNormalize(cat_cols)
